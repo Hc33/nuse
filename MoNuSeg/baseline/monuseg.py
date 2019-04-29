986056e84474f7d6c8caa3baa375758348efc0ce
Yufei Liu <yufeiliu94@gmail.com>
2019-04-29 13:28:37 +0800

    :construction: added evaluation code
    - added evaluation code
    - fixed rotation

--- MoNuSeg/baseline/monuseg.py
+++ MoNuSeg/baseline/monuseg.py
@@ -2,22 +2,25 @@
 
 import random
 import torch
+import numpy as np
 from torch.utils.data import Dataset
 import torchvision.transforms as tr
 import torchvision.transforms.functional as fn
 
 
 class ByPass:
-    def __call__(self, image):
-        return image
+    def __call__(self, *args):
+        return args
 
 
 class Rotation:
     def __init__(self, degree):
         self.degree = degree
 
-    def __call__(self, image):
-        return fn.rotate(image, self.degree)
+    def __call__(self, image, mask):
+        image = fn.rotate(image, self.degree)
+        mask = np.rot90(mask, k=self.degree // 90)
+        return image, np.ascontiguousarray(mask)
 
 
 class RandomRotate:
@@ -31,10 +34,7 @@ class RandomRotate:
 
     def __call__(self, image, mask):
         transform = random.choice(self.choices)
-        image = transform(image)
-        mask = fn.to_pil_image(mask)
-        mask = transform(mask)
-        mask = fn.to_tensor(mask)
+        image, mask = transform(image, mask)
         return image, mask
 
 
@@ -53,15 +53,27 @@ class MoNuSegTransform:
     def __init__(self):
         self.crop = RandomCrop()
         self.rot = RandomRotate()
-        self.norm = tr.Normalize(mean=[0.7198306, 0.43624926, 0.5741127], std=[0.13865258, 0.18120667, 0.14840752])
+        self.norm = tr.Normalize(mean=[0.7198306, 0.43624926, 0.5741127],
+                                 std=[0.13865258, 0.18120667, 0.14840752])
 
     def __call__(self, image, mask):
         patch, label = self.crop(image, mask)
-        #patch, label = self.rot(patch, label)
+        patch, label = self.rot(patch, label)
         patch = self.norm(fn.to_tensor(patch))
 
         return patch, torch.from_numpy(label).long()
 
+
+class MoNuSegTestTransform:
+    def __init__(self):
+        self.norm = tr.Normalize(mean=[0.7198306, 0.43624926, 0.5741127],
+                                 std=[0.13865258, 0.18120667, 0.14840752])
+        self.pad = tr.Pad(12, padding_mode='reflect')
+
+    def __call__(self, image, mask):
+        image = self.norm(fn.to_tensor(self.pad(image)))
+        mask = torch.from_numpy(mask[::2, ::2]).long()
+        return image, mask
 
 
 class MoNuSeg(Dataset):
@@ -79,8 +91,10 @@ class MoNuSeg(Dataset):
             pid = by_organ['Breast'][:4] + by_organ['Liver'][:4] + by_organ['Kidney'][:4] + by_organ['Prostate'][:4]
         elif same_organ_testing:
             pid = by_organ['Breast'][4:] + by_organ['Liver'][4:] + by_organ['Kidney'][4:] + by_organ['Prostate'][4:]
+            multiplier = 1
         elif different_organ_testing:
             pid = by_organ['Bladder'] + by_organ['Colon'] + by_organ['Stomach']
+            multiplier = 1
         else:
             raise ValueError(error_message)
 
@@ -88,6 +102,8 @@ class MoNuSeg(Dataset):
         self.is_training = training
         if self.is_training:
             self.transform = MoNuSegTransform()
+        else:
+            self.transform = MoNuSegTestTransform()
         self.multiplier = multiplier
 
     def __len__(self):
--- MoNuSeg/train_fcn.py
+++ MoNuSeg/train_fcn.py
@@ -8,8 +8,51 @@ from baseline.fcn import FCN
 from baseline.monuseg import MoNuSeg
 
 
-def train(num_epoch, device):
+def universal_test(model, device, loader, organs):
+    model.eval()
+    hypot_record, label_record = [], []
+    for organ, (image, label) in zip(organs, loader):
+        with torch.no_grad():
+            image = image.to(device)
+            label = label.to(device)
+            hypot = model(image).max(dim=1)[1][:, 6:506, 6:506]
+            currect = (hypot == label).sum().item()
+            hypot_record.append(currect)
+            label_record.append(label.numel())
+            print(f'Accuracy of {organ:8s} = {currect / label.numel():.4f}')
+    print(f'Overall Accuracy = {sum(hypot_record) / sum(label_record):.4f}')
+    model.train()
+
+
+def same_organ_test(model, device):
+    dataset = MoNuSeg('monuseg.pth', training=False, same_organ_testing=True)
+    loader = DataLoader(dataset, batch_size=2)
+    organs = ['Breast', 'Liver', 'Kidney', 'Prostate']
+    universal_test(model, device, loader, organs)
+
+
+def different_organ_test(model, device):
+    dataset = MoNuSeg('monuseg.pth', training=False, different_organ_testing=True)
+    loader = DataLoader(dataset, batch_size=2)
+    organs = ['Bladder', 'Colon', 'Stomach']
+    universal_test(model, device, loader, organs)
+
+
+def all_test(model, device):
+    print('=' * 20, 'SAME ORGAN TEST', '=' * 20)
+    same_organ_test(model, device)
+    print('=' * 18, 'DIFFERENT ORGAN TEST', '=' * 17)
+    different_organ_test(model, device)
+    print('=' * 57)
+
+
+def train(num_epoch, device, recover=None):
     model = FCN().cuda(device=device)
+    if recover is not None:
+        print('Loading', recover)
+        snapshot = torch.load(recover, 'cpu')
+        model.load_state_dict(snapshot)
+        all_test(model, device)
     model.train()
     optimizer = torch.optim.Adadelta(model.parameters(), lr=0.5)
     dataset = MoNuSeg('monuseg.pth', training=True)
@@ -29,13 +72,19 @@ def train(num_epoch, device):
                 loss = loss.item()
                 accuracy = (hypot.max(dim=1)[1] == truth).long().sum().item() / label.numel()
 
-            print(f'Epoch {epoch:4d} Iteration {iteration:4d} loss = {loss:.4f} accuracy = {accuracy:.4f} {label.float().mean() / 2}')
-        if epoch % 16:
+            print(f'Epoch {epoch:4d} Iteration {iteration:4d} loss = {loss:.4f} accuracy = {accuracy:.4f}')
+        if epoch % 16 == 0:
             torch.save(model.state_dict(), f'snapshot/{epoch}.pth')
+            all_test(model, device)
 
 
 def main():
-    train(num_epoch=1000, device=0)
+    import sys
+    if len(sys.argv) == 2:
+        recover = sys.argv[-1]
+    else:
+        recover = None
+    train(num_epoch=1024, device=0, recover=recover)
 
 
 if __name__ == '__main__':
